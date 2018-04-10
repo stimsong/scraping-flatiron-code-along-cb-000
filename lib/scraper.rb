093995c8e3efc17daf5c0a73ac87a2100319b583
Greg Stimson <stimsong@yahoo.com>
2018-04-10 23:06:40 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,8 +5,11 @@ require 'pry'
 require_relative './course.rb'
 
 class Scraper
-  
-    def print_courses
+
+  def get_page
+  end
+
+  def print_courses
     self.make_courses
     Course.all.each do |course|
       if course.title
@@ -16,8 +19,5 @@ class Scraper
       end
     end
   end
-  
+
 end
-
-
-
